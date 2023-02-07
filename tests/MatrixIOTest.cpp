133215c5f9b1b7a0a5c4a0c0d826d68c43ff0dd4
Gonc Berk Gunes <goncberk@gmail.com>
2023-02-07 14:45:23 +0100

    complete exercise

---
+++ tests/MatrixIOTest.cpp
@@ -0,0 +1,30 @@
+#define BOOST_TEST_DYN_LINK
+#include <Eigen/Dense>
+#include <boost/test/unit_test.hpp>
+#include "matrixIO.hpp"
+
+using namespace Eigen;
+
+BOOST_AUTO_TEST_SUITE(MatrixIOTests)
+
+BOOST_AUTO_TEST_CASE(printData)
+{
+  MatrixXd  A, expectedA;
+  std::string       test;
+
+  A = MatrixXd(3, 3);
+  A << 1, 2, 3,
+      4, 5, 6,
+      7, 8, 9;
+
+  matrixIO::saveData("../csvText.csv", A);
+  expectedA = matrixIO::openData("../csvText.csv", 3);
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      BOOST_TEST(A(i, j) == expectedA(i, j));
+    }
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
